895ba03a102449c1e4808648667c2fc3c9aa1019
Shahadat <shahadat2cp17@gmail.com>
2022-03-13 15:58:55 +0600

    cloud

--- cloud/src/components/Navbar.jsx
+++ cloud/src/components/Navbar.jsx
@@ -3,7 +3,11 @@ import { MenuIcon, XIcon } from '@heroicons/react/outline'
 const Navbar = () => {
     return (
         <div className='w-screen h-[75px] bg-zinc-200 fixed drop-shadow-lg'>
-            <div className='px-2 flex justify-between items-center w-full h-full'></div>
+            <div className='px-2 flex justify-between items-center w-full h-full'>
+                <div className='flex items-center'>
+                    <h1 className='text-3xl font-bold mr-4 sm:text-4xl'>SONAIMURI.</h1>
+                </div>
+            </div>
             <MenuIcon className='w-7' />
         </div>
     )
